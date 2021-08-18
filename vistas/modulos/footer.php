d2e13f4eab1073bac42d7f69aafc1afda4d3cff0
SalvadorChumbeCordova <salvdor290915@gmail.com>
2021-08-18 12:20:50 -0500

    first commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 <a href="https://www.compartiendocodigos.net/" target="_blank">FIIS-UNAS</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
